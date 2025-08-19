32c5c7d5211a79006de8c163c44fa484c99f48f9
Sagar <sagarsahu5976@gmail.com>
2025-08-19 23:52:00 +0530

    changed social links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -95,9 +95,9 @@ const Index = () => {
             
             <div className="flex justify-center gap-6">
               {[
-                { icon: Github, label: "GitHub", href: "https://github.com" },
-                { icon: Linkedin, label: "LinkedIn", href: "https://linkedin.com" },
-                { icon: Mail, label: "Email", href: "mailto:contact@skillchain.dev" }
+                { icon: Github, label: "GitHub", href: "https://github.com/CodeByAshuu" },
+                { icon: Linkedin, label: "LinkedIn", href: "https://www.linkedin.com/in/sagarrrr/" },
+                { icon: Mail, label: "Email", href: "https://mail.google.com/mail/u/0/#inbox?compose=DmwnWsmHbFSZFjqrcBCsnWpdTFJnvJZHKhgHtTBvSXplBqGQhsGSqLwScxhcbQWPHtxMVcsdQkBB" }
               ].map(({ icon: Icon, label, href }) => (
                 <a
                   key={label}
@@ -181,13 +181,13 @@ const Index = () => {
           
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <Button size="lg" asChild>
-              <a href="mailto:contact@skillchain.dev">
+              <a href="https://mail.google.com/mail/u/0/#inbox?compose=DmwnWsmHbFSZFjqrcBCsnWpdTFJnvJZHKhgHtTBvSXplBqGQhsGSqLwScxhcbQWPHtxMVcsdQkBB">
                 <Mail className="w-5 h-5 mr-2" />
                 Get In Touch
               </a>
             </Button>
             <Button variant="outline" size="lg" asChild>
-              <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+              <a href="https://github.com/CodeByAshuu/SkillVault" target="_blank" rel="noopener noreferrer">
                 <Github className="w-5 h-5 mr-2" />
                 View GitHub
               </a>
